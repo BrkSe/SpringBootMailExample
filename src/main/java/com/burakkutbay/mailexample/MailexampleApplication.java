6e48c92c804366fb945ec258514a9102733e4f7e
BrkSe <brkktby@gmail.com>
2018-06-22 13:48:35 +0300

    CommandLine runner method created.

--- src/main/java/com/burakkutbay/mailexample/MailexampleApplication.java
+++ src/main/java/com/burakkutbay/mailexample/MailexampleApplication.java
@@ -1,12 +1,23 @@
 package com.burakkutbay.mailexample;
 
+import com.burakkutbay.mailexample.controller.MailController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MailexampleApplication {
+public class MailexampleApplication implements CommandLineRunner {
+
+    @Autowired
+    private MailController mailController;
 
     public static void main(String[] args) {
         SpringApplication.run(MailexampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        mailController.sendMail();
     }
 }
