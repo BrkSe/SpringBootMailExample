ea4d3ffa77fe18a2f9ba0024754d4dcfccb47447
BrkSe <brkktby@gmail.com>
2018-06-22 13:48:05 +0300

    sendMail method.

--- src/main/java/com/burakkutbay/mailexample/controller/MailController.java
+++ src/main/java/com/burakkutbay/mailexample/controller/MailController.java
@@ -1,4 +1,32 @@
 package com.burakkutbay.mailexample.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Controller;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Controller
 public class MailController {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public String sendMail() {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+
+        try {
+            messageHelper.setTo("emailadress");
+            messageHelper.setText("Greetings :)");
+            messageHelper.setSubject("Mail From Spring Boot");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "Error...";
+        }
+        mailSender.send(mimeMessage);
+        return "Mail Sent!";
+    }
 }
