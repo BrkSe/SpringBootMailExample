c5cb1a61f14cd4d1b594ab214813665dfd0561d9
BrkSe <brkktby@gmail.com>
2018-06-22 13:26:17 +0300

    Initial commit

---
+++ src/main/java/com/burakkutbay/mailexample/MailexampleApplication.java
@@ -0,0 +1,12 @@
+package com.burakkutbay.mailexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailexampleApplication.class, args);
+    }
+}
